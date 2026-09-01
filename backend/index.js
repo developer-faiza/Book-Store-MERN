6675a6cf55b8b37310ce0e6a62967b3f73993a4f
Faiza Memon <developerfaizamemon@gmail.com>
2026-09-01 12:40:58 +0500

    Fix formatting of CORS middleware options

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin:true
+    origin:true,
     credentials: true,
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type']
